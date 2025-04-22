156f0119e10f17c797e0379b5e5c9fd50cbb925a
Amr <legithamouda@gmail.com>
2025-04-22 19:22:28 +0200

    test(Ship): add failing tests for isSunk method

--- src/Ship.test.js
+++ src/Ship.test.js
@@ -10,4 +10,15 @@ describe("Ship", () => {
   test("increases number of hits", () => {
     expect(babyShip.getHits()).toBe(5);
   });
+
+  test("considers the ship is sunk if hits equals ship's length", () => {
+    expect(babyShip.isSunk()).toBe(true);
+  });
+  test("returns false if hits is less than length", () => {
+    const shippy = Ship(4);
+    shippy.hit();
+    shippy.hit();
+    shippy.hit();
+    expect(shippy.isSunk()).toBe(false);
+  });
 });
