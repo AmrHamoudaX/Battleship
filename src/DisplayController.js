86993bdbffe9658f8ed588e2abcc8ca7ae8c2b3a
Amr <legithamouda@gmail.com>
2025-04-28 15:19:08 +0300

    feat(DisplayController): Renders the game logic to the UI

---
+++ src/DisplayController.js
@@ -0,0 +1,48 @@
+export { DisplayController };
+import { CreateUiGrid } from "./CreateUiGrid";
+import { GameController } from "./GameController";
+import { RenderBoard } from "./RenderBoard";
+
+function DisplayController() {
+  CreateUiGrid();
+  const game = GameController();
+  const playerTurnDiv = document.querySelector(".turn");
+  const playerBoardDiv = document.querySelector(".my-board");
+  const enemyBoardDiv = document.querySelector(".enemy-board");
+  console.log(enemyBoardDiv);
+
+  const updateScreen = () => {
+    //get the newest version of the board and player turn
+    const activePlayer = game.getActivePlayer();
+    const playerBoard = activePlayer.gameBoard.board;
+    const enemyBoard = game.getEnemyPlayer().gameBoard.board;
+
+    //Display player's turn
+    playerTurnDiv.textContent = `${activePlayer.playerName}'s turn...`;
+
+    RenderBoard(playerCells, playerBoard, true);
+    RenderBoard(enemyCells, enemyBoard, false);
+
+    //We dont need to return anything from this module because everything is encapsulated inside this screen controller
+  };
+  const playerCells = [...document.querySelectorAll(".my-cells")];
+  const enemyCells = [...document.querySelectorAll(".enemy-cells")];
+  //Add event listener for the board
+  enemyCells.forEach((cell) =>
+    cell.addEventListener("click", clickHandlerBoard),
+  );
+  function clickHandlerBoard(e) {
+    const selectedRow = e.target.dataset.row;
+    const selectedColumn = e.target.dataset.col;
+    console.log(selectedRow, selectedColumn);
+    //Make sure I've clicked a column and not the gaps in between
+    if (!selectedRow || !selectedColumn) return;
+
+    game.playRound(selectedRow, selectedColumn);
+    updateScreen();
+    console.log(selectedRow);
+    console.log(selectedColumn);
+  }
+  //initial render
+  updateScreen();
+}
