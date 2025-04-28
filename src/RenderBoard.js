32c67df1693c4e5201743047e245842955f602f4
Amr <legithamouda@gmail.com>
2025-04-28 22:02:35 +0300

    fix(RenderBoard): now works for when isPlayer returns false

--- src/RenderBoard.js
+++ src/RenderBoard.js
@@ -1,37 +1,35 @@
 export { RenderBoard };
 
 function RenderBoard(cells, board, isPlayerBoard) {
-  if (isPlayerBoard) {
-    //Show ships
-    for (let row = 0; row < 10; row++) {
-      for (let col = 0; col < 10; col++) {
-        const index = row * 10 + col;
-        const cell = cells[index];
-        const value = board[col][row];
+  //Show ships
+  for (let row = 0; row < 10; row++) {
+    for (let col = 0; col < 10; col++) {
+      const index = col * 10 + row;
+      const cell = cells[index];
+      const value = board[row][col];
 
-        //Clear previous classes
-        cell.className = "";
-        cell.classList.add("cell");
+      //Clear previous classes
+      cell.className = "";
+      cell.classList.add("cell");
 
-        //Always set the dataset attributes
+      //Always set the dataset attributes
 
-        cells[index].dataset.row = row;
-        cells[index].dataset.col = col;
+      cells[index].dataset.row = row;
+      cells[index].dataset.col = col;
 
-        if (value === ".") {
-          //empty water
-        } else if (value === "X") {
-          cell.classList.add("miss");
-        } else if (value === "O") {
-          cell.classList.add("hit");
+      if (value === ".") {
+        //empty water
+      } else if (value === "X") {
+        cell.classList.add("miss");
+      } else if (value === "O") {
+        cell.classList.add("hit");
+      } else {
+        //This is a ship
+        if (isPlayerBoard) {
+          cell.classList.add("has-ship");
         } else {
-          //This is a ship
-          if (isPlayerBoard) {
-            cell.classList.add("has-ship");
-          } else {
-            cell.classList.add("fogged");
-            //If enemy board, hide ships!
-          }
+          cell.classList.add("fogged");
+          //If enemy board, hide ships!
         }
       }
     }
